56e57aef8c2ad2bb382d24fda53e12513af31165
Matvey Gasnikov <gasnikov.ma@gmail.com>
2024-04-20 23:25:03 +0300

    hw10 final(2)

--- bot/src/main/java/edu/java/bot/Executor.java
+++ bot/src/main/java/edu/java/bot/Executor.java
@@ -7,20 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 @org.springframework.stereotype.Controller
 public class Executor {
 
-    private final Service telegramBotService;
+    private final Service telegraBotService;
     private final TelegramBot bot;
 
     @Autowired
     public Executor(Service service, TelegramBot bot) {
-        this.telegramBotService = service;
+        this.telegraBotService = service;
         this.bot = bot;
         this.run();
     }
 
     public void run() {
-        telegramBotService.setMyCommands();
+        telegraBotService.setMyCommands();
         bot.setUpdatesListener(list -> {
-            list.forEach(telegramBotService::handleUpdates);
+            list.forEach(telegraBotService::handleUpdates);
 
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
 
